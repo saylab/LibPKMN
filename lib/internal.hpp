d18e80197b5dd363976c6c0387ff458180843815
Nicholas Corgan <n.corgan@gmail.com>
2014-09-11 09:38:24 -0700

    Brought over paths.cpp.in and internal.hpp from spinda branch

---
+++ lib/internal.hpp
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2014 Nicholas Corgan (n.corgan@gmail.com)
+ *
+ * Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
+ * or copy at http://opensource.org/licenses/MIT)
+ */
+#ifndef INCLUDED_INTERNAL_HPP
+#define INCLUDED_INTERNAL_HPP
+
+#include <cstdint>
+
+#include <pkmn/config.hpp>
+
+namespace pkmn
+{
+    /*
+     * This is an abstraction for getting the value of an environment
+     * variable, using the UNIX or Windows functions as appropriate.
+     */
+    std::string PKMN_INLINE getenv(const char* var)
+    {
+        std::string value;
+
+        #ifdef PKMN_PLATFORM_WIN32
+        char* arr;
+        size_t len;
+        _dupenv_s(&arr, &len, var);
+        if(!arr) return std::string("");
+        else
+        {
+            value = std::string(arr);
+            free(arr);
+            return value;
+        }
+        #else
+        const char* arr = getenv(var);
+        return arr ? std::string(arr) : std::string("");
+        #endif
+    }
+}
+
+#endif /* INCLUDED_INTERNAL_HPP */
