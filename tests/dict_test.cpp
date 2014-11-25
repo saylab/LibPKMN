a3fed3a2322157ed4cfc7faac30b93d7530bc8ee
Nicholas Corgan <n.corgan@gmail.com>
2014-11-25 15:19:13 -0800

    Made dict_test more robust

--- tests/dict_test.cpp
+++ tests/dict_test.cpp
@@ -16,63 +16,95 @@
  * but it is not portable, so a custom #define is necessary. If this doesn't
  * work, then the Boostless pkmn::dict class will not work.
  */
-BOOST_AUTO_TEST_CASE(pkmn_dict_has_key)
+BOOST_AUTO_TEST_CASE(has_key_test)
 {
-    const pkmn::dict<int, int> b_dict = boost::assign::map_list_of
+    pkmn::dict<int, int> d = boost::assign::map_list_of
         (0,-1)
         (2,5)
         (4,57)
     ;
-    BOOST_CHECK(b_dict.has_key(0));
-    BOOST_CHECK(not b_dict.has_key(3));
+    BOOST_CHECK(d.has_key(0));
+    BOOST_CHECK(not d.has_key(3));
 }
 
 /*
- * Test getting and setting with pkmn::dict's [] operator
+ * Testing both at() functions
  */
-BOOST_AUTO_TEST_CASE(pkmn_dict_operator)
+BOOST_AUTO_TEST_CASE(at_test)
 {
-    pkmn::dict<int, int> b_dict = boost::assign::map_list_of
+    pkmn::dict<int, int> d = boost::assign::map_list_of
         (0,-1)
         (2,5)
         (4,57)
     ;
-    int test_var = b_dict[0];
-    BOOST_CHECK_EQUAL(test_var, -1);
-
-    b_dict[4] = 10;
-    BOOST_CHECK_EQUAL(b_dict[4], 10);
-
-    b_dict[10] = 7;
-    BOOST_CHECK(b_dict.has_key(10));
-    BOOST_CHECK_EQUAL(b_dict.at(10,11), 7);
+    BOOST_CHECK_EQUAL(d.at(2), 5);
+    BOOST_CHECK_EQUAL(d.at(3,10), 10);
 }
 
 /*
- * Testing both pkmn::dict::at() functions
+ * Test getting and setting with [] operator
  */
-BOOST_AUTO_TEST_CASE(pkmn_dict_at)
+BOOST_AUTO_TEST_CASE(operator_test)
 {
-    const pkmn::dict<int, int> b_dict = boost::assign::map_list_of
-        (0,-1)
-        (2,5)
-        (4,57)
-    ;
-    BOOST_CHECK_EQUAL(b_dict.at(2), 5);
-    BOOST_CHECK_EQUAL(b_dict.at(3,10), 10);
+    pkmn::dict<int, int> d;
+
+    // Test set operator
+    d[5] = 10;
+    BOOST_CHECK_EQUAL(d.at(5,5), 10);
+
+    // Test get operator
+    BOOST_CHECK_EQUAL(d[5], 10);
 }
 
 /*
- * Testing whether pkmn::dict::swap() swaps
+ * Testing whether swap() swaps
  */
-BOOST_AUTO_TEST_CASE(pkmn_dict_swap)
+BOOST_AUTO_TEST_CASE(swap_test)
 {
-    pkmn::dict<int, int> b_dict = boost::assign::map_list_of
+    pkmn::dict<int, int> d = boost::assign::map_list_of
         (0,-1)
         (2,5)
         (4,57)
     ;
-    b_dict.swap(0,4);
-    BOOST_CHECK_EQUAL(b_dict[0], 57);
-    BOOST_CHECK_EQUAL(b_dict[4], -1);
+    d.swap(0,4);
+    BOOST_CHECK_EQUAL(d[0], 57);
+    BOOST_CHECK_EQUAL(d[4], -1);
+}
+
+/*
+ * Test whether erase() erases
+ * Test whether empty() properly checks emptiness.
+ */
+BOOST_AUTO_TEST_CASE(erase_empty_test)
+{
+    pkmn::dict<int, int> d;
+    BOOST_CHECK(d.empty());
+    d[5] = 10;
+    BOOST_CHECK(not d.empty());
+    d.clear();
+    BOOST_CHECK(d.empty());
+}
+
+/*
+ * Test whether keys() and vals() work properly
+ */
+BOOST_AUTO_TEST_CASE(keys_vals_test)
+{
+    pkmn::dict<int, int> d = boost::assign::map_list_of
+        (0,-1)
+        (2,5)
+        (4,57)
+    ;
+
+    std::vector<int> keys = d.keys();
+    BOOST_CHECK_EQUAL(keys.size(), 3);
+    BOOST_CHECK_EQUAL(keys[0], 0);
+    BOOST_CHECK_EQUAL(keys[1], 2);
+    BOOST_CHECK_EQUAL(keys[2], 4);
+
+    std::vector<int> vals = d.vals();
+    BOOST_CHECK_EQUAL(vals.size(), 3);
+    BOOST_CHECK_EQUAL(vals[0], -1);
+    BOOST_CHECK_EQUAL(vals[1], 5);
+    BOOST_CHECK_EQUAL(vals[2], 57);
 }
