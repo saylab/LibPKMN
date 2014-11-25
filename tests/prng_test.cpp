6d269b58895fad9ee07d31854c30e5383890ff84
Nicholas Corgan <n.corgan@gmail.com>
2014-11-24 23:41:55 -0800

    tests: improved prng_test
    * Added tests for ARNG and MTRNG
    * Fixed uninitialized value exposed by MTRNG test

--- tests/prng_test.cpp
+++ tests/prng_test.cpp
@@ -9,7 +9,27 @@
 
 #include <pkmn/types/prng.hpp>
 
-BOOST_AUTO_TEST_CASE(check_lcrng)
+/*
+ * Make sure the ARNG returns the same result from the same seed.
+ */
+BOOST_AUTO_TEST_CASE(arng_test)
+{
+    pkmn::prng::sptr rng = pkmn::prng::make(3);
+
+    uint32_t arng_val1 = rng->arng();
+    rng->arng();
+    uint32_t arng_val2 = rng->arng();
+
+    rng->seed_arng(arng_val1);
+    rng->arng();
+    BOOST_CHECK_EQUAL(rng->arng(), arng_val2);
+}
+
+/*
+ * Make sure Generation IV LCRNG values are 32-bit numbers.
+ * Make sure the LCRNG returns the same result from the same seed.
+ */
+BOOST_AUTO_TEST_CASE(lcrng_test)
 {
     pkmn::prng::sptr rng4 = pkmn::prng::make(4);
     pkmn::prng::sptr rng5 = pkmn::prng::make(5);
@@ -17,7 +37,7 @@ BOOST_AUTO_TEST_CASE(check_lcrng)
     uint64_t lcrng4_val1 = rng4->lcrng();
     uint64_t lcrng5_val1 = rng5->lcrng();
 
-    //Make sure the Gen IV LCRNG seed is a valid 32-bit value
+    // Make sure the Gen IV LCRNG seed is a valid 32-bit value
     BOOST_CHECK(lcrng4_val1 < 4294967295);
 
     rng4->lcrng();
@@ -26,7 +46,7 @@ BOOST_AUTO_TEST_CASE(check_lcrng)
     uint64_t lcrng4_val2 = rng4->lcrng();
     uint64_t lcrng5_val2 = rng5->lcrng();
 
-    //Run the check again
+    // Run the check again
     BOOST_CHECK(lcrng4_val2 < 4294967295);
 
     rng4->seed_lcrng(lcrng4_val1);
@@ -35,7 +55,19 @@ BOOST_AUTO_TEST_CASE(check_lcrng)
     rng4->lcrng();
     rng5->lcrng();
 
-    //Make sure PRNG recreates value with same seed
+    // Make sure PRNG recreates value with same seed
     BOOST_CHECK_EQUAL(rng4->lcrng(), lcrng4_val2);
     BOOST_CHECK_EQUAL(rng5->lcrng(), lcrng5_val2);
+}
+
+/*
+ * Make sure the MTRNG array resets when it should.
+ */
+BOOST_AUTO_TEST_CASE(mtrng_test)
+{
+    pkmn::prng::sptr rng = pkmn::prng::make(3);
+
+    uint32_t mtrng_val = rng->mtrng();
+    for(size_t i = 0; i < 623; i++) rng->mtrng();
+    BOOST_CHECK(rng->mtrng() != mtrng_val);
 }
