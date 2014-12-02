b0f6ff44f59b8e1f9ad1c0df1ef1948e09f2edd4
Nicholas Corgan <n.corgan@gmail.com>
2014-12-01 19:14:09 -0800

    tests: added C++ and C# tests for uint8_t <-> pkmn::markings conversions

---
+++ tests/CSharpMarkingsTest.cs
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2014 Nicholas Corgan (n.corgan@gmail.com)
+ *
+ * Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
+ * or copy at http://opensource.org/licenses/MIT)
+ */
+
+public class DictTest
+{
+    public static void ToMarkingsTest()
+    {
+        try
+        {
+            LibPKMN.Markings mark = new LibPKMN.Markings();
+            mark.circle = true;
+            mark.triangle = false;
+            mark.square = true;
+            mark.heart = false;
+            mark.star = true;
+            mark.diamond = false;
+
+            byte markInt = mark;
+            if(markInt != 21)
+                throw new System.Exception("markInt (" + markInt + ") != 0b010101");
+        }
+        catch(System.Exception ex)
+        {
+            System.Console.WriteLine("Caught exception:");
+            System.Console.WriteLine("   " + ex.Message);
+            System.Console.WriteLine(ex.StackTrace);
+            System.Environment.Exit(1);
+        }
+    }
+
+    public static void FromMarkingsTest()
+    {
+        try
+        {
+            byte markInt = 21;
+            LibPKMN.Markings mark = markInt;
+
+            if(!mark.circle)
+                throw new System.Exception("mark.circle != true");
+            if(mark.triangle)
+                throw new System.Exception("mark.triangle != false");
+            if(!mark.square)
+                throw new System.Exception("mark.square != true");
+            if(mark.heart)
+                throw new System.Exception("mark.heart != false");
+            if(!mark.star)
+                throw new System.Exception("mark.star != true");
+            if(mark.diamond)
+                throw new System.Exception("mark.diamond != false");
+        }
+        catch(System.Exception ex)
+        {
+            System.Console.WriteLine("Caught exception:");
+            System.Console.WriteLine("   " + ex.Message);
+            System.Console.WriteLine(ex.StackTrace);
+            System.Environment.Exit(1);
+        }
+    }
+
+    public static int Main(string[] args)
+    {
+        ToMarkingsTest();
+        FromMarkingsTest();
+
+        return 0;
+    }    
+}
---
+++ tests/markings_test.cpp
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2014 Nicholas Corgan (n.corgan@gmail.com)
+ *
+ * Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
+ * or copy at http://opensource.org/licenses/MIT)
+ */
+
+#include <string>
+
+#include <boost/test/unit_test.hpp>
+
+#include <pkmn/types/markings.hpp>
+
+BOOST_AUTO_TEST_CASE(from_markings_test)
+{
+    pkmn::markings mark;
+    mark.circle = true;
+    mark.triangle = false;
+    mark.square = true;
+    mark.heart = false;
+    mark.star = true;
+    mark.diamond = false;
+
+    uint8_t mark_int = mark;
+    BOOST_CHECK_EQUAL(mark_int, 0b010101);
+}
+
+BOOST_AUTO_TEST_CASE(to_markings_test)
+{
+    uint8_t mark_int = 0b010101;
+    pkmn::markings mark = mark_int;
+    BOOST_CHECK(mark.circle);
+    BOOST_CHECK(!mark.triangle);
+    BOOST_CHECK(mark.square);
+    BOOST_CHECK(!mark.heart);
+    BOOST_CHECK(mark.star);
+    BOOST_CHECK(!mark.diamond);
+}
