55b3f560d0d48dc1a52f8c11a6882c397d91e36c
Nicholas Corgan <n.corgan@gmail.com>
2014-12-03 13:08:56 -0800

    conversions: added test conversion functions for Gen IV+

--- lib/conversions/text.hpp
+++ lib/conversions/text.hpp
@@ -116,6 +116,12 @@ namespace pkmn
 
         pkmn::pkstring PKMN_API import_gen3_text(const uint8_t* buffer, uint8_t max_len);
         void PKMN_API export_gen3_text(const pkmn::pkstring &text, uint8_t* buffer, uint8_t max_len);
+
+        pkmn::pkstring PKMN_API import_gen4_text(const uint16_t* buffer, uint8_t max_len);
+        void PKMN_API export_gen4_text(const pkmn::pkstring &text, uint16_t* buffer, uint8_t max_len);
+
+        pkmn::pkstring PKMN_API import_modern_text(const uint16_t* buffer, uint8_t max_len);
+        void PKMN_API export_modern_text(const pkmn::pkstring &text, uint16_t* buffer, uint8_t max_len);
     }
 }
 #endif /* INCLUDED_PKMN_CONVERSIONS_TEXT_HPP */
