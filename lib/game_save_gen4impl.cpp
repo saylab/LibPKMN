6ff05d13046da3fdad31a9ad12f42bab2f0845f1
Nicholas Corgan <n.corgan@gmail.com>
2014-12-16 14:42:22 -0800

    game_save: started refactoring Gen IV implementation

--- lib/game_save_gen4impl.cpp
+++ lib/game_save_gen4impl.cpp
@@ -9,7 +9,8 @@
 
 #include "game_save_gen4impl.hpp"
 
-#include "conversions/trainer.hpp"
+#include "conversions/items.hpp"
+#include "conversions/text.hpp"
 
 namespace fs = boost::filesystem;
 
@@ -18,19 +19,47 @@ namespace pkmn
     game_save_gen4impl::game_save_gen4impl(pokelib_sptr ptr,
                                            const std::string &filename): game_save_impl(filename)
     {
-        _pokelib_save = ptr;
+    }
+
+    game_save_gen4impl::game_save_gen4impl(const std::string &filename,
+                                           gen4_games_t game):
+        game_save_impl(filename),
+        _game_type(game),
+        _game_id(libpkmn_version_ids[game])
+    {
         load();
     }
 
+    #define GEN4_DATA(arr,name)           (&arr[gen4_offsets[name][_game_type]])
+    #define GEN4_DATA_CAST(arr,name,type) (reinterpret_cast<type*>(GEN4_DATA(arr,name)))
+
     void game_save_gen4impl::load()
     {
-        _trainer = conversions::import_gen4_trainer(_pokelib_save);
-        _game_id = _trainer->get_game_id();
+        _blocks[A]  = GEN4_DATA(_data, gen4_blockA);
+        _blocks[B]  = GEN4_DATA(_data, gen4_blockB);
+        _footers[A] = GEN4_DATA_CAST(_data, gen4_footerA, gen4_footer_t);
+        _footers[B] = GEN4_DATA_CAST(_data, gen4_footerB, gen4_footer_t);
+
+        _party      = GEN4_DATA_CAST(_blocks[A], gen4_party, nds_pokemon_party_t);
+        _items      = GEN4_DATA_CAST(_blocks[A], gen4_items, void);
+
+        _trainer = trainer::make(_game_id,
+                                 conversions::import_gen4_text(GEN4_DATA_CAST(_data, gen4_name, uint16_t), 8),
+                                 *GEN4_DATA(_data, gen4_gender) ? Genders::FEMALE : Genders::MALE);
+
+        for(size_t i = 0; i < _party->count; i++)
+        {
+            _trainer->set_pokemon((i+1), conversions::import_nds_pokemon(_party->party[i],
+                                                                         _game_id));
+        }
+        conversions::import_gen4_bag(_trainer->get_bag(), _items);
+
+        _trainer->set_id(*GEN4_DATA_CAST(_data, gen4_tid, uint32_t));
+        _trainer->set_money(*GEN4_DATA_CAST(_data, gen4_money, uint32_t));
     }
 
     void game_save_gen4impl::save_as(const std::string &filename)
     {
-        conversions::export_gen4_trainer(_trainer, _pokelib_save);
         //TODO: actual saving stuff
         _filepath = fs::path(filename);
     }
