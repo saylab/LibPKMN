cd3ad8c60ad0eb1ef72c5e2d5209ff165000dbba
Nicholas Corgan <n.corgan@gmail.com>
2015-01-24 21:21:13 -0800

    Brought bag back

--- lib/bag_impl.hpp
+++ lib/bag_impl.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2014 Nicholas Corgan (n.corgan@gmail.com)
+ * Copyright (c) 2013-2015 Nicholas Corgan (n.corgan@gmail.com)
  *
  * Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
  * or copy at http://opensource.org/licenses/MIT)
@@ -17,38 +17,34 @@ namespace pkmn
     {
         public:
 
-            bag_impl(unsigned int game);
+            bag_impl(uint16_t game);
             bag_impl(const bag_impl& other);
             bag_impl& operator=(const bag_impl& other);
             ~bag_impl() {};
 
             pkmn::pkstring get_game() const;
-            unsigned int get_generation() const;
+            uint16_t get_generation() const;
 
-            void add_item(const pkmn::pkstring &item_name, unsigned int amount);
-            void add_item(unsigned int item_id, unsigned int amount);
-            void add_item(item::sptr item_sptr, unsigned int amount);
-            void remove_item(const pkmn::pkstring &item_name, unsigned int amount);
-            void remove_item(unsigned int item_id, unsigned int amount);
-            void remove_item(item::sptr item_sptr, unsigned int amount);
-            unsigned int get_item_amount(const pkmn::pkstring &item_name) const;
-            unsigned int get_item_amount(unsigned int item_id) const;
-            unsigned int get_item_amount(item::sptr item_sptr) const;
+            void add_item(const pkmn::pkstring &item_name, uint16_t amount);
+            void add_item(uint16_t item_id, uint16_t amount);
+            void remove_item(const pkmn::pkstring &item_name, uint16_t amount);
+            void remove_item(uint16_t item_id, uint16_t amount);
+            uint16_t get_item_amount(const pkmn::pkstring &item_name) const;
+            uint16_t get_item_amount(uint16_t item_id) const;
 
             pocket::sptr get_pocket(const pkmn::pkstring &name) const;
             pkmn::dict<pkmn::pkstring, pocket::sptr> get_pockets() const;
 
-            unsigned int get_game_id() const;
+            uint16_t get_game_id() const;
 
         private:
 
             static pkmn::shared_ptr<SQLite::Database> _db;
 
-            unsigned int _game_id, _generation;
+            uint16_t _game_id, _generation;
             pkmn::dict<pkmn::pkstring, pocket::sptr> _pockets; 
 
-            pkmn::pkstring _get_pocket_name(unsigned int item_id) const;
-            void _check() const;
+            pkmn::pkstring _get_pocket_name(uint16_t item_id) const;
     };
 }
 
