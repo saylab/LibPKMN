3e5984b89385a6dd9bffb130ec44057802315f6f
Nicholas Corgan <n.corgan@gmail.com>
2015-04-03 21:03:11 -0700

    pksql_test: finished equality check function

--- tests/pksql_test.cpp
+++ tests/pksql_test.cpp
@@ -9,6 +9,7 @@
 #include <string>
 
 #include <boost/filesystem.hpp>
+#include <boost/foreach.hpp>
 #include <boost/format.hpp>
 #include <boost/test/unit_test.hpp>
 
@@ -18,7 +19,7 @@
 
 namespace fs = boost::filesystem;
 
-static void pksql_common_equality_check(pkmn::pokemon::sptr pkmn1, pkmn::pokemon::sptr pkmn2)
+static void pksql_equality_check(pkmn::pokemon::sptr pkmn1, pkmn::pokemon::sptr pkmn2)
 {
     BOOST_CHECK_EQUAL(pkmn1->get_species_id(), pkmn2->get_species_id());
     BOOST_CHECK_EQUAL(pkmn1->get_game_id(), pkmn2->get_game_id());
@@ -49,6 +50,67 @@ static void pksql_common_equality_check(pkmn::pokemon::sptr pkmn1, pkmn::pokemon
     BOOST_CHECK_EQUAL(pkmn1->get_experience(), pkmn2->get_experience());
     BOOST_CHECK(pkmn1->get_gender() == pkmn2->get_gender());
     BOOST_CHECK_EQUAL(pkmn1->is_shiny(), pkmn2->is_shiny());
+
+    pkmn::dict<pkmn::pkstring, int> pkmn1_stats = pkmn1->get_stats();
+    pkmn::dict<pkmn::pkstring, int> pkmn2_stats = pkmn2->get_stats();
+    BOOST_CHECK_EQUAL(pkmn1_stats.at("HP"), pkmn2_stats.at("HP"));
+    BOOST_CHECK_EQUAL(pkmn1_stats.at("Attack"), pkmn2_stats.at("Attack"));
+    BOOST_CHECK_EQUAL(pkmn1_stats.at("Defense"), pkmn2_stats.at("Defense"));
+    BOOST_CHECK_EQUAL(pkmn1_stats.at("Speed"), pkmn2_stats.at("Speed"));
+
+    if(pkmn1->get_generation() == 1)
+        BOOST_CHECK_EQUAL(pkmn1_stats.at("Special"), pkmn2_stats.at("Special"));
+    else
+    {
+        BOOST_CHECK_EQUAL(pkmn1_stats.at("Special Attack"), pkmn2_stats.at("Special Attack"));
+        BOOST_CHECK_EQUAL(pkmn1_stats.at("Special Defense"), pkmn2_stats.at("Special Defense"));
+    }
+
+    pkmn::dict<pkmn::pkstring, int> pkmn1_EVs = pkmn1->get_EVs();
+    pkmn::dict<pkmn::pkstring, int> pkmn2_EVs = pkmn2->get_EVs();
+    BOOST_CHECK_EQUAL(pkmn1_EVs.at("HP"), pkmn2_EVs.at("HP"));
+    BOOST_CHECK_EQUAL(pkmn1_EVs.at("Attack"), pkmn2_EVs.at("Attack"));
+    BOOST_CHECK_EQUAL(pkmn1_EVs.at("Defense"), pkmn2_EVs.at("Defense"));
+    BOOST_CHECK_EQUAL(pkmn1_EVs.at("Speed"), pkmn2_EVs.at("Speed"));
+
+    if(pkmn1->get_generation() < 3)
+        BOOST_CHECK_EQUAL(pkmn1_EVs.at("Special"), pkmn2_EVs.at("Special"));
+    else
+    {
+        BOOST_CHECK_EQUAL(pkmn1_EVs.at("Special Attack"), pkmn2_EVs.at("Special Attack"));
+        BOOST_CHECK_EQUAL(pkmn1_EVs.at("Special Defense"), pkmn2_EVs.at("Special Defense"));
+    }
+
+    pkmn::dict<pkmn::pkstring, int> pkmn1_IVs = pkmn1->get_IVs();
+    pkmn::dict<pkmn::pkstring, int> pkmn2_IVs = pkmn2->get_IVs();
+    BOOST_CHECK_EQUAL(pkmn1_IVs.at("HP"), pkmn2_IVs.at("HP"));
+    BOOST_CHECK_EQUAL(pkmn1_IVs.at("Attack"), pkmn2_IVs.at("Attack"));
+    BOOST_CHECK_EQUAL(pkmn1_IVs.at("Defense"), pkmn2_IVs.at("Defense"));
+    BOOST_CHECK_EQUAL(pkmn1_IVs.at("Speed"), pkmn2_IVs.at("Speed"));
+
+    if(pkmn1->get_generation() < 3)
+        BOOST_CHECK_EQUAL(pkmn1_IVs.at("Special"), pkmn2_IVs.at("Special"));
+    else
+    {
+        BOOST_CHECK_EQUAL(pkmn1_IVs.at("Special Attack"), pkmn2_IVs.at("Special Attack"));
+        BOOST_CHECK_EQUAL(pkmn1_IVs.at("Special Defense"), pkmn2_IVs.at("Special Defense"));
+    }
+
+    pkmn::moveset_t moves1, moves2;
+    pkmn1->get_moves(moves1);
+    pkmn2->get_moves(moves2);
+    BOOST_CHECK(moves1[0].name == moves2[0].name);
+    BOOST_CHECK(moves1[1].name == moves2[1].name);
+    BOOST_CHECK(moves1[2].name == moves2[2].name);
+    BOOST_CHECK(moves1[3].name == moves2[3].name);
+
+    pkmn::dict<pkmn::pkstring, int> pkmn1_attributes = pkmn1->get_attributes();
+    pkmn::dict<pkmn::pkstring, int> pkmn2_attributes = pkmn2->get_attributes();
+    BOOST_CHECK_EQUAL(pkmn1_attributes.size(), pkmn2_attributes.size());
+    BOOST_FOREACH(const pkmn::pkstring &key, pkmn1_attributes.keys())
+    {
+        BOOST_CHECK_EQUAL(pkmn1_attributes.at(key), pkmn1_attributes.at(key,(pkmn1_attributes.at(key)-1)));
+    }
 }
 
 BOOST_AUTO_TEST_CASE(gen1_pksql_test)
@@ -65,7 +127,7 @@ BOOST_AUTO_TEST_CASE(gen1_pksql_test)
     pkmn::pokemon::export_to(pkmn1, filepath.string());
     pkmn::pokemon::sptr pkmn2 = pkmn::pokemon::make(filepath.string());
 
-    pksql_common_equality_check(pkmn1, pkmn2);
+    pksql_equality_check(pkmn1, pkmn2);
 
     fs::remove(filepath);
 }
@@ -82,7 +144,7 @@ BOOST_AUTO_TEST_CASE(gen2_pksql_test)
     pkmn::pokemon::export_to(pkmn1, filepath.string());
     pkmn::pokemon::sptr pkmn2 = pkmn::pokemon::make(filepath.string());
 
-    pksql_common_equality_check(pkmn1, pkmn2);
+    pksql_equality_check(pkmn1, pkmn2);
 
     fs::remove(filepath);
 }
