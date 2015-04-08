40a2e1c2fe65e72203291d008de46722deeb8a00
Nicholas Corgan <n.corgan@gmail.com>
2015-04-08 10:11:25 -0700

    pksql: set non-default values to test

--- tests/pksql_test.cpp
+++ tests/pksql_test.cpp
@@ -121,6 +121,26 @@ BOOST_AUTO_TEST_CASE(gen1_pksql_test)
                                                     "Tackle", "Vine Whip",
                                                     "Sleep Powder", "Razor Leaf");
 
+    // Set stats to non-default
+    pkmn1->set_nickname("GEN1_PKSQL");
+    pkmn1->set_trainer_name("WAT");
+    pkmn1->set_trainer_id(01351);
+    pkmn1->set_level(7);
+    pkmn1->set_EV("HP", 692);
+    pkmn1->set_EV("Attack", 2135);
+    pkmn1->set_EV("Defense", 846);
+    pkmn1->set_EV("Speed", 100);
+    pkmn1->set_EV("Special", 8);
+    pkmn1->set_IV("HP", 10);
+    pkmn1->set_IV("Attack", 2);
+    pkmn1->set_IV("Defense", 3);
+    pkmn1->set_IV("Speed", 4);
+    pkmn1->set_IV("Special", 15);
+    pkmn1->set_move("Solar Beam", 1);
+    pkmn1->set_move("Stun Spore", 4);
+    pkmn1->set_move_PP(10, 2);
+    pkmn1->set_move_PP(5, 3);
+
     std::string filename = str(boost::format("pksql_gen1_%d.pksql") % rand());
     fs::path filepath = fs::path(fs::path(pkmn::get_tmp_dir()) / filename);
 
@@ -137,6 +157,27 @@ BOOST_AUTO_TEST_CASE(gen2_pksql_test)
     pkmn::pokemon::sptr pkmn1 = pkmn::pokemon::make("Chikorita", "Gold", 50,
                                                     "Tackle", "Vine Whip",
                                                     "Sleep Powder", "Razor Leaf");
+
+    // Set stats to non-default
+    pkmn1->set_nickname("GEN2_PKSQL");
+    pkmn1->set_trainer_name("WAT");
+    pkmn1->set_trainer_id(01351);
+    pkmn1->set_level(70);
+    pkmn1->set_EV("HP", 692);
+    pkmn1->set_EV("Attack", 2135);
+    pkmn1->set_EV("Defense", 846);
+    pkmn1->set_EV("Speed", 100);
+    pkmn1->set_EV("Special", 8);
+    pkmn1->set_IV("HP", 10);
+    pkmn1->set_IV("Attack", 2);
+    pkmn1->set_IV("Defense", 3);
+    pkmn1->set_IV("Speed", 4);
+    pkmn1->set_IV("Special", 15);
+    pkmn1->set_move("Solar Beam", 1);
+    pkmn1->set_move("Stun Spore", 4);
+    pkmn1->set_move_PP(10, 2);
+    pkmn1->set_move_PP(5, 3);
+    pkmn1->set_gender("Female");
 
     std::string filename = str(boost::format("pksql_gen2_%d.pksql") % rand());
     fs::path filepath = fs::path(fs::path(pkmn::get_tmp_dir()) / filename);
@@ -155,6 +196,29 @@ BOOST_AUTO_TEST_CASE(gen3_pksql_test)
                                                     "Tackle", "Vine Whip",
                                                     "Sleep Powder", "Razor Leaf");
 
+    // Set stats to non-default
+    pkmn1->set_nickname("GEN3_PKSQL");
+    pkmn1->set_trainer_name("WAT");
+    pkmn1->set_trainer_id(13511351);
+    pkmn1->set_level(40);
+    pkmn1->set_EV("HP", 200);
+    pkmn1->set_EV("Attack", 25);
+    pkmn1->set_EV("Defense", 46);
+    pkmn1->set_EV("Speed", 10);
+    pkmn1->set_EV("Special Attack", 38);
+    pkmn1->set_EV("Special Defense", 38);
+    pkmn1->set_IV("HP", 10);
+    pkmn1->set_IV("Attack", 2);
+    pkmn1->set_IV("Defense", 3);
+    pkmn1->set_IV("Speed", 4);
+    pkmn1->set_IV("Special Attack", 15);
+    pkmn1->set_IV("Special Defense", 12);
+    pkmn1->set_move("Solar Beam", 1);
+    pkmn1->set_move("Slash", 4);
+    pkmn1->set_move_PP(10, 2);
+    pkmn1->set_move_PP(5, 3);
+    pkmn1->set_gender("Female");
+
     std::string filename = str(boost::format("pksql_gen3_%d.pksql") % rand());
     fs::path filepath = fs::path(fs::path(pkmn::get_tmp_dir()) / filename);
 
@@ -172,6 +236,29 @@ BOOST_AUTO_TEST_CASE(gen4_pksql_test)
                                                     "Tackle", "Vine Whip",
                                                     "Sleep Powder", "Razor Leaf");
 
+    // Set stats to non-default
+    pkmn1->set_nickname("GEN4_PKSQL");
+    pkmn1->set_trainer_name("WAT");
+    pkmn1->set_trainer_id(13511351);
+    pkmn1->set_level(40);
+    pkmn1->set_EV("HP", 200);
+    pkmn1->set_EV("Attack", 25);
+    pkmn1->set_EV("Defense", 46);
+    pkmn1->set_EV("Speed", 10);
+    pkmn1->set_EV("Special Attack", 38);
+    pkmn1->set_EV("Special Defense", 38);
+    pkmn1->set_IV("HP", 10);
+    pkmn1->set_IV("Attack", 2);
+    pkmn1->set_IV("Defense", 3);
+    pkmn1->set_IV("Speed", 4);
+    pkmn1->set_IV("Special Attack", 15);
+    pkmn1->set_IV("Special Defense", 12);
+    pkmn1->set_move("Solar Beam", 1);
+    pkmn1->set_move("Earthquake", 4);
+    pkmn1->set_move_PP(10, 2);
+    pkmn1->set_move_PP(5, 3);
+    pkmn1->set_gender("Female");
+
     std::string filename = str(boost::format("pksql_gen4_%d.pksql") % rand());
     fs::path filepath = fs::path(fs::path(pkmn::get_tmp_dir()) / filename);
 
@@ -188,6 +275,29 @@ BOOST_AUTO_TEST_CASE(gen5_pksql_test)
     pkmn::pokemon::sptr pkmn1 = pkmn::pokemon::make("Snivy", "White", 50,
                                                     "Tackle", "Vine Whip",
                                                     "Sleep Powder", "Razor Leaf");
+
+    // Set stats to non-default
+    pkmn1->set_nickname("GEN5_PKSQL");
+    pkmn1->set_trainer_name("WAT");
+    pkmn1->set_trainer_id(13511351);
+    pkmn1->set_level(100);
+    pkmn1->set_EV("HP", 200);
+    pkmn1->set_EV("Attack", 25);
+    pkmn1->set_EV("Defense", 46);
+    pkmn1->set_EV("Speed", 10);
+    pkmn1->set_EV("Special Attack", 38);
+    pkmn1->set_EV("Special Defense", 38);
+    pkmn1->set_IV("HP", 10);
+    pkmn1->set_IV("Attack", 2);
+    pkmn1->set_IV("Defense", 3);
+    pkmn1->set_IV("Speed", 4);
+    pkmn1->set_IV("Special Attack", 15);
+    pkmn1->set_IV("Special Defense", 12);
+    pkmn1->set_move("Solar Beam", 1);
+    pkmn1->set_move("Earthquake", 4);
+    pkmn1->set_move_PP(10, 2);
+    pkmn1->set_move_PP(5, 3);
+    pkmn1->set_gender("Female");
 
     std::string filename = str(boost::format("pksql_gen5_%d.pksql") % rand());
     fs::path filepath = fs::path(fs::path(pkmn::get_tmp_dir()) / filename);
